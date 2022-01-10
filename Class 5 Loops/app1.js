91c1e4731850480a4583a3654d182196b65701c7
MilchoK <96262988+MilchoK@users.noreply.github.com>
2022-01-10 15:14:39 +0100

    Homework

---
+++ Class 5 Loops/app1.js
@@ -0,0 +1,9 @@
+array = ["Hello", "there", "students", "of", "SEDC", "!"];
+
+function printArray(array) {
+  for (const item of array) {
+    console.log(item);
+  }
+}
+
+printArray(array);
---
+++ Class 5 Loops/app2.js
@@ -0,0 +1,8 @@
+for (let i = 1; i < 21; i++) {
+  if (i % 2 == 0) {
+    console.log(i);
+    console.log("\n");
+  } else {
+    console.log(i + " ");
+  }
+}
