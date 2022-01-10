c1cd2ac65e9e9208021e29bdfb0f3d837a1308b9
MilchoK <96262988+MilchoK@users.noreply.github.com>
2022-01-10 13:01:32 +0100

    Homework part 1
    Homework part 1

---
+++ Class 3 Functions/app#1.js
@@ -0,0 +1,16 @@
+function findItemType(item){
+    console.log(typeof item);
+}
+
+let item1 = 1<2;
+let item2 = "String";
+let item3 = 123;
+let item4 = undefined;
+let item5 = {};
+
+
+findItemType(item1);
+findItemType(item2);
+findItemType(item3);
+findItemType(item4);
+findItemType(item5);
--- README.md
+++
@@ -1,3 +0,0 @@
-var price = 119.95;
-var Total = 30 * price * 1.05;
-console.log("The total cost is:" + Total);
